b91d8408c8ac33828164b996a323093b99c5267a
lrenee <lrenee.mo@sbcglobal.net>
2019-10-20 18:09:08 -0500

    end of 28.7.2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Cool Pics';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://livability.com/sites/default/files/Florida-Secret-Beaches.jpg';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7wmSP3dfSt9y2vi3Ic1-9vqsAdXEDavqMuDUU_n4rmtcdmXjP0g';
 
   constructor() { }
 
